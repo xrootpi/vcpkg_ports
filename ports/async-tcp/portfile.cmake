2374d25b5a323df9f810c91c075fb50f664b43d5
Mohammad Kashem <mohammad.kashem@outlook.co.nz>
2018-12-25 02:07:04 +1300

    updated sha512 for v0.0.2

--- ports/async-tcp/portfile.cmake
+++ ports/async-tcp/portfile.cmake
@@ -1,15 +1,3 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
 
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/async_tcp)
@@ -17,8 +5,8 @@ set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/async_tcp)
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO xrootpi/async_tcp
-	REF  v0.0.1
-	SHA512 b4c18b8c4bf04d645857905312d6876aadd3b4b632878d1d75eb8e0e39125e2fba80f0536c46a69ffe229fbddf168a6e1262aad70ec806b8c3fd8855ccad496d
+	REF v0.0.2
+	SHA512 314b4ab114051fd13a450d96c85c7b009e9ef8df59e7cf5b3fae0a9d16fbe3fe2db25600ade263bbe6eb0b911b742aaecece874f7c2628bbe5406293a832ad25
     )
 
 vcpkg_configure_cmake(
