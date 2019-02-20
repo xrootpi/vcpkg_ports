1e1551306f2d765f456eee9f1d206d96b279bd1e
Mohammad Kashem <mohammad.kashem@outlook.co.nz>
2019-02-20 17:10:09 +1300

    update websocket_server to v0.0.4

--- ports/websocket-server/portfile.cmake
+++ ports/websocket-server/portfile.cmake
@@ -5,8 +5,8 @@ set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/websocket_server)
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO xrootpi/websocket_server
-	REF v0.0.3
-	SHA512 0a27a68c3bc6a6386c7bbcb19ceefc7956c46e71ea30e82a576e49a03124e7c73bfe6a5d73c8f96a579ae1ab5c758d8d4782e252081f2b95225b68e5c44831c0
+	REF v0.0.4
+	SHA512 b56ccafea9923e4cf0b032ec3c4ede92564c20ab3655adce8b29063758b0b678d04c4bf0dd780c97442219fb9eb68fd3965fdfdcb1f41eec3788c985b77062ca
     )
 
 vcpkg_configure_cmake( 
@@ -14,7 +14,7 @@ vcpkg_configure_cmake(
 	PREFER_NINJA
 )
 
-file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/websocket-server)
+file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/websocket_server)
 
 file(INSTALL ${SOURCE_PATH}/license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/websocket-server RENAME copyright)
 
