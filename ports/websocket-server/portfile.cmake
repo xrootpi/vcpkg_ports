908e5539094b4c1b8e60cb4a31a5896a5737c1f3
Mohammad Kashem <mohammad.kashem@outlook.co.nz>
2019-07-28 00:54:06 +1200

    update package info

--- ports/websocket-server/portfile.cmake
+++ ports/websocket-server/portfile.cmake
@@ -5,8 +5,8 @@ set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/websocket_server)
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO xrootpi/websocket_server
-	REF v0.0.8
-	SHA512 a86d67bb12c15a9f78a47fe5585435dec7f3761ddedf990566f69379e19ccc31d9766ea36ddeef9120eab112c1188f2658aa0b76ab5f8e52435dcf58e2bbce90
+	REF v0.0.9
+	SHA512 c838fd0158ca14d000113592d7ca7770b1be05cd5e6c8f12275044aa5de15bd73fa30990c2f8d448b3fb6e30421bad8314489eb5191a9b1bd08e54cc04f70662
     )
 
 vcpkg_configure_cmake( 
